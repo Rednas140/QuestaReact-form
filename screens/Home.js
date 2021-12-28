e535f7a60126329342b2bca9c45f4d458fe029ee
Sander <sanderjverhoeven@hotmail.com>
2021-12-28 18:42:38 +0100

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Button } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function Home({ navigation }) {
+    return (
+        <View style={styles.background}>
+            {/* Weergave van de huidige in te vullen vragenlijst */}
+            <Text style={styles.mainText}>
+                Rotterdam
+            </Text>
+            <TouchableOpacity 
+            onPress={() => navigation.navigate('Question')}
+            style={[styles.mainButton, styles.shadowProp, styles.elevation]}>
+                <Text style={styles.mainText2}>
+                    Ga naar je huidige vragenlijst!
+                </Text>
+            </TouchableOpacity>
+            {/* Weergave van de nog in te vullen vragenlijsten */}
+            <Text style={styles.toFillText}>
+                Nog in te vullen
+            </Text>
+            <TouchableOpacity 
+            onPress={() => navigation.navigate('Question')}
+            style={[styles.toFillButton, styles.shadowProp, styles.elevation]}>
+                <Text style={styles.toFillText2}>
+                    Ga naar je nog in te vullen vragenlijst!
+                </Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+            onPress={() => navigation.navigate('Question')}
+            style={[styles.toFillButton, styles.shadowProp, styles.elevation]}>
+                <Text style={styles.toFillText2}>
+                    Ga naar je nog in te vullen vragenlijst!
+                </Text>
+            </TouchableOpacity>
+
+            {/* Weergave van de ingevulde vragenlijsten */}
+            <Text style={styles.filledText}>
+                Ingevuld
+            </Text>
+            <TouchableOpacity 
+            onPress={() => navigation.navigate('Question')}
+            style={[styles.filledButton, styles.shadowProp, styles.elevation]}>
+                <Text style={styles.filledText2}>
+                    Ga naar je nog in te vullen vragenlijst!
+                </Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+            onPress={() => navigation.navigate('Question')}
+            style={[styles.filledButton, styles.shadowProp, styles.elevation]}>
+                <Text style={styles.filledText2}>
+                    Ga naar je nog in te vullen vragenlijst!
+                </Text>
+            </TouchableOpacity>
+        </View>  
+    );
+    
+    }
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "center",
+        backgroundColor: "#2B50EC",
+    },
+
+    mainText:{
+        marginTop: 10,
+        fontSize: 40,
+        color: "#fff",
+    },
+
+    mainButton:{
+        marginTop: 10,
+        width: "90%",
+        height: 80,
+        backgroundColor: "#111329",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 5,
+    },
+
+    mainText2:{
+        color: "#fff",
+        fontSize: 22,
+    },
+
+    toFillText:{
+        marginTop: 20,
+        fontSize: 30,
+        color: "#fff",
+    },
+
+    toFillButton:{
+        marginTop: 10,
+        width: "90%",
+        height: 80,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 5,
+    },
+
+    toFillText2:{
+        color: "#111329",
+        fontSize: 22,
+    },
+
+    filledText:{
+        marginTop: 20,
+        fontSize: 30,
+        color: "#fff",
+    },
+
+    filledButton:{
+        marginTop: 10,
+        width: "90%",
+        height: 80,
+        backgroundColor: "#1CA883",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 5,
+    },
+
+    filledText2:{
+        color: "#fff",
+        fontSize: 22,
+    },
+
+    shadowProp:{
+        shadowColor: 'black',
+        shadowOffset: {width: -2, height: 4},
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+    },
+
+    elevation: {
+        elevation: 5,
+        shadowColor: 'black',
+      },
+})
+
+export default Home;
---
+++ screens/Question2.js
@@ -0,0 +1,202 @@
+import React, { useState, useEffect } from 'react';
+import { Text, TextInput, View, StyleSheet, Image, TouchableOpacity, ScrollView, Alert, Pressable, Button } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Speech from 'expo-speech';
+
+
+function Question2({ navigation }) { 
+    const [shouldShow, setShouldShow] = useState(null);
+    const [shouldShowText, setShouldShowText] = useState(null);
+
+    //tts
+    // const speak = () => {
+    //     const thingToSay = 'Hoe was je ervaring van Rotterdam Centraal?';
+    //     Speech.speak(thingToSay, {language: "nl-NL",});
+    //     console.log(Speech.getAvailableVoicesAsync)
+    //   };
+
+    //setting the state for the camera and file sorting
+    const [image, setImage] = useState(null);
+    const [imageCam, setImageCam] = useState(null);
+
+    //the alert for asking to take a picture of pick a photo from your storage
+    const alertPhoto = () =>
+    Alert.alert(
+      "Kies je upload methode",
+      "Kies een al genomen foto, of neem een foto via de app",
+      [
+        {
+          text: "Kies",
+          onPress: () => pickImage(),
+        },
+        { text: "Neem", 
+        onPress: () => takeImage(), 
+        }
+      ]
+    );
+    // checking permissions
+    useEffect(() => {
+      (async () => {
+        if (Platform.OS !== 'web') {
+          const { status } = await ImagePicker.getCameraPermissionsAsync(); 
+        }
+      })();
+    }, []);
+  
+    //the function for picking an image from your files
+    const pickImage = async () => {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+  
+      console.log(result);
+  
+      if (!result.cancelled) {
+        setImage(result.uri);
+      }
+    };
+
+    //the function for taking a picture
+    const takeImage = async () => {
+        let result2 = await ImagePicker.launchCameraAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+  
+      console.log(result2);
+  
+      if (!result2.cancelled) {
+        setImageCam(result2.uri);
+      }
+    };
+    return (
+      <ScrollView contentContainerStyle={styles.ScrollView}>
+          <View style={styles.questionBackground}>
+              <Text style={styles.questionTitle}>
+                  Vraag 2
+              </Text>
+              <Text style={styles.questionBody}>
+                Is voor jou duidelijk wat je kunt kopen en hoe duur het is? 
+              </Text>
+              {shouldShow ? ( <Image style={styles.microphoneRed} source={require('../assets/icons/microphoneRed.png')} /> ) : true}
+          </View>
+            {shouldShowText ? ( <TextInput style={styles.inputField} placeholder="Vul hier je antwoord in" /> ) : true}
+          <View style={styles.iconRow}>
+
+              <TouchableOpacity
+              accessibilityLabel='Foto Nemen' 
+              onPress={alertPhoto}>
+                <Image style={styles.camera}  source={require('../assets/icons/camera.png')} />
+              </TouchableOpacity>
+
+              <TouchableOpacity 
+              accessibilityLabel='Tekstveld openen' 
+              onPressIn={() => setShouldShowText(!shouldShowText)}>
+                <Image style={styles.font} source={require('../assets/icons/font.png')} />
+              </TouchableOpacity>
+
+              <TouchableOpacity 
+              accessibilityLabel='Geluid Opnemen' 
+              onPressIn={() => setShouldShow(!shouldShow)} onPressOut={() => setShouldShow(!shouldShow)}>
+                <Image style={styles.microphone} source={require('../assets/icons/microphone.png')} />
+              </TouchableOpacity>
+
+          </View>
+          <Pressable style={styles.buttonNext} onPress={() => navigation.navigate('Question3')}>
+            <Text style={styles.textButton}>Volgende Vraag</Text>
+          </Pressable>
+          <View style={styles.iconRow}>
+              {imageCam && <Image source={{ uri: imageCam }} style={{ width: 200, height: 200 }} />}
+              {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+          </View>
+      </ScrollView>
+        );
+}
+
+const styles = StyleSheet.create({
+    ScrollView: {
+        flexGrow: 1,
+        maxHeight: "200%",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        backgroundColor: "#fff",
+        overflow: "scroll",
+    },
+    questionBackground:{
+        width: "100%",
+        height: "50%",
+        backgroundColor: "#2B50EC",
+        minHeight: "50%",
+    },
+    questionTitle:{
+        textAlign: "center",
+        paddingTop: 40,
+        fontSize: 30,
+        color: "white",
+    },
+    questionBody:{
+        textAlign: "center",
+        paddingTop: 75,
+        fontSize: 30,
+        color: "white",
+    },
+    inputField:{
+        paddingLeft: 8,
+        marginTop: 20,
+        borderColor: "#111329",
+        borderWidth: 2,
+        borderRadius: 3,
+        width: "80%",
+    },
+    iconRow:{
+        flexDirection: 'row',
+        marginTop: 10,
+    },
+    buttonNext:{
+      marginTop: 30,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      borderRadius: 4,
+      elevation: 3,
+      width: "80%",
+      backgroundColor: '#2B50EC',
+    },
+    textButton:{
+      fontSize: 16,
+      lineHeight: 21,
+      letterSpacing: 0.25,
+      color: 'white',
+    },
+    camera:{
+        width: 50,
+        height: 50,
+        marginRight: 25,
+    },
+    font:{
+      width: 50,
+      height: 50,
+      marginRight: 25,
+      marginLeft: 25,
+  },
+    microphone:{
+        width: 50,
+        height: 50,
+        marginLeft: 25,
+    },
+    microphoneRed:{
+      marginTop: 20,
+      width: 50,
+      height: 50,
+      justifyContent: 'center',
+      alignSelf: 'center',  
+  },
+})
+
+export default Question2;	
